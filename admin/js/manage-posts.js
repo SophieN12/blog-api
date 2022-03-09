7a558837cf6d7c3b4eece0760356e0fddad69ec8
SophieN12 <Sophie-nhan@hotmail.com>
2022-03-09 04:11:46 +0100

    Added tags to admin page

--- admin/js/manage-posts.js
+++ admin/js/manage-posts.js
@@ -3,37 +3,49 @@ window.onload = function() {
 }
 
 async function fetchAllPosts() {
-    let response = await fetch("http://localhost:5000/posts");
-    let data = await response.json();
-
-    let postsHTML = ""
-
-    for (let post of data){
-        postsHTML += `<li> <div> 
-                        <b> Title </b>: ${post.title} <br> 
-                        <b> Author: </b> ${post.author} <br> 
-                        <b> Date: </b> ${post.date} <br> </div>
-                        <div class="manage-post" >
-                            <a href="update-post.html"> Update </a>
-                            <a href="#" data-id= ${post._id}> Delete </a>
-                        </div>
-                    </li>`
-    }
-
-    document.getElementById("posts-list").innerHTML = postsHTML;
-
-    let deleteLinks = document.querySelectorAll("#posts-list li a:last-child");
-    for (let link of deleteLinks) {
-        link.addEventListener("click", async function (e) {
-            e.preventDefault();
-
-            try{
-                await fetch("http://localhost:5000/posts/"+ e.target.dataset.id, {method: "DELETE"});
-                e.target.parentNode.parentNode.remove();
-            } catch (err){
-                console.log(err);
+    try{
+        let response = await fetch("http://localhost:5000/posts");
+        let data = await response.json();
+    
+        let postsHTML = "";
+        let postTags = "";
+    
+        for (let post of data){
+            if (post.tags === null || post.tags.length == 0) {
+                postTags = "None";
+            } else {
+                postTags = post.tags;
             }
-        })
+    
+            postsHTML += `<li> <div> 
+                            <b> Title </b>: ${post.title} <br> 
+                            <b> Author: </b> ${post.author} <br> 
+                            <b> Date: </b> ${post.date} <br> 
+                            <b> Tags: </b> ${postTags} <br> </div>
+                            <div class="manage-post" >
+                                <a href="update-post.html"> Update </a>
+                                <a href="#" data-id= ${post._id}> Delete </a>
+                            </div>
+                        </li>`
+        }
+    
+        document.getElementById("posts-list").innerHTML = postsHTML;
+    
+        let deleteLinks = document.querySelectorAll("#posts-list li a:last-child");
+        for (let link of deleteLinks) {
+            link.addEventListener("click", async function (e) {
+                e.preventDefault();
+    
+                try{
+                    await fetch("http://localhost:5000/posts/"+ e.target.dataset.id, {method: "DELETE"});
+                    e.target.parentNode.parentNode.remove();
+                } catch (err){
+                    console.log(err);
+                }
+            })
+        }
+    } catch(err) {
+        console.log(err);
     }
 }
 
