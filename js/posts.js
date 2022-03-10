025968d45399e9c80fde48b9ec7a7d820165a758
Natalie Esseen <natalieesseen@gmail.com>
2022-03-10 15:35:38 +0100

    cleaned up for loop

--- js/posts.js
+++ js/posts.js
@@ -12,20 +12,25 @@ async function fetchAllPosts() {
 
         console.log(posts)
 
-        for(var i = 0, l = posts.length; i < l; i++) {
-            blogPosts.innerHTML += `
-            <div id="post-entry">
-            <h2>${posts[i].title}</h2>
-            <h3>${posts[i].author}</h3>
-            <i>${posts[i].date}</i>
-            <p>${posts[i].content}</p>
-            <i>${posts[i].tags}</i>
-            </div>
+        let html = ''
+        for(let post of posts) {
+
+            html += `
+            <li class="list-group-item">
+            <h2>${post.title}</h2>
+            <i>${post.author} | ${post.date}</i>
+            <p><b>tags: </b>${post.tags}</p>
+            <p>${post.content.substring(0, 100)} ... <a href="/post.html?id=${post._id}">läs mer</a></p>
+
+            </li>
             `
-            // endast visa 100 första från content
-            // på tags, lägga till mellanrum for varje ','
+
+            // hur separera taggar?
+            // bör man ha en if-sats om blogginlägg har < 100 karaktärer?
+            
         }
 
+        blogPosts.innerHTML = html;
         
     } catch(error) {
         console.log(error);
